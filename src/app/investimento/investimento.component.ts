bad2e654c3e6f6404272b92fbced9977d5a4436e
DavidWCQ1 <david.wcq@gmail.com>
2020-11-18 20:27:43 -0300

    refec

--- src/app/investimento/investimento.component.ts
+++ src/app/investimento/investimento.component.ts
@@ -10,8 +10,18 @@ import { InvestimentoService } from './investimento.service'
 export class InvestimentoComponent implements OnInit {
 
   response = []
+  acao = []
+  isAcao = false
+  multiMercado = []
+  isMultiMercado = false
+  jurosPos = []
+  isJuros = false
+  cambial = []
+  isCambial = false
+  inflacao = []
+  isInflacao = false
+  status = true
   
-
 
   constructor(
     private investimentoService: InvestimentoService
@@ -21,7 +31,60 @@ export class InvestimentoComponent implements OnInit {
     this.investimentoService.consultaInvestimento()
     .subscribe(respoonse => {
       this.response = respoonse["data"]
+
+      this.acao = this.response.filter(data =>{
+        return data.benchmarkClass == "Ações"
+      })
+
+      this.multiMercado = this.response.filter(data =>{
+        return data.benchmarkClass == "Multimercados"
+      })
+
+      this.cambial = this.response.filter(data =>{
+        return data.benchmarkClass == "Cambial"
+      })
+
+      this.jurosPos = this.response.filter(data =>{
+        return data.benchmarkClass == "Juros Pós"
+      })
+
+      this.inflacao = this.response.filter(data =>{
+        return data.benchmarkClass == "Inflação"
+      })
     })
   }
 
+  showAcao(){
+    this.isAcao = true
+    this.status = false
+  }
+
+  showMultiMercado(){
+    this.isMultiMercado = true
+    this.status = false
+  }
+
+  showCambial(){
+    this.isCambial = true
+    this.status = false
+  }
+
+  showJuros(){
+    this.isJuros = true
+    this.status = false
+  }
+
+  showInflacao(){
+    this.isInflacao = true
+    this.status = false
+  }
+  
+  goBack(){
+    this.status = true
+    this.isInflacao = false
+    this.isAcao = false
+    this.isCambial = false
+    this.isMultiMercado = false
+    this.isJuros = false
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false ,
+
+  GET_TOKEN : '/api',
+
+  EXTRATO: 'https://sandbox.openbanking.original.com.br/accounts/v1/balance',
+  
+  FATURA: 'https://sandbox.openbanking.original.com.br/cards/v1/cards',
+
+  TRANSFERENCIA: 'https://sandbox.openbanking.original.com.br/money-transfers/v1/transfers',
+
+  PAGAMENTOS_DE_BARRAS_CONSULTA: 'https://sandbox.openbanking.original.com.br/payments/v3/slips/infos',
+
+  PAGAMENTOS_DE_BARRAS_PAGAR: 'https://sandbox.openbanking.original.com.br/payments/v3/slips',
+
+  GERAR_BOLETOS: 'https://sandbox.openbanking.original.com.br/slips/v1/slips',
+
+  INVESTIMENTO: 'https://sandbox.openbanking.original.com.br/investments/v1/investments'
+
 };
 
 /*
